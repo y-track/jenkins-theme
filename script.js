9cee3af2d1c8a76b4fd0b4bcc8d0e7aae82a0fea
Paul Varache <perso@paulvarache.ninja>
2015-07-21 11:26:27 +0200

    Image

--- script.js
+++ script.js
@@ -1,2 +1,4 @@
-var icon = document.querySelector('#jenkins-head-icon');
-icon.src = 'http://paulvarache.github.io/jenkins-theme/images/dmp-logo.png';
+document.addEventListener('DOMContentReady', function () {
+    var icon = document.querySelector('#jenkins-head-icon');
+    icon.src = 'http://paulvarache.github.io/jenkins-theme/images/dmp-logo.png';
+});
