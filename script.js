3b0eb9637715699924deb4e17c0640e5b4a5c1e6
Thomas Papiernik <thomas.papiernik@gmail.com>
2016-01-05 17:26:57 +0100

    Fix logo path

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 document.addEventListener('DOMContentLoaded', function () {
     var icon = document.querySelector('#jenkins-head-icon');
-    icon.src = 'images/dmp-logo.png';
+    icon.src = 'http://y-track.github.io/jenkins-theme/images/dmp-logo.png';
 });
